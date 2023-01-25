c17e765e457fa2eda5fa22c6ffcf57e6fdd616d9
Matt <39172048+magiccreb@users.noreply.github.com>
2023-01-25 14:42:46 -0500

    Update to connection.js.

--- Develop/db/connection.js
+++ Develop/db/connection.js
@@ -5,8 +5,8 @@ const connection = mysql.createConnection({
   // Your username
   user: "root",
   // Your password
-  password: "rootroot",
-  database: "employees"
+  password: "root",
+  database: "meepmoop"
 });
 
 connection.connect(function (err) {
